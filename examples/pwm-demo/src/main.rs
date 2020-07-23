017dc5ebb7209cca3c21157676e53f004c186966
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2020-07-23 12:49:35 +0200

    main.rs: make it compiler (linker error to be resolved)

--- examples/pwm-demo/src/main.rs
+++ examples/pwm-demo/src/main.rs
@@ -9,8 +9,11 @@ use cortex_m_rt::entry;
 
 // Provides definitions for our development board
 use dwm1001::{
+    prelude::*,
     DWM1001,
 };
+
+use nb::block;
 
 use nrf52832_hal::{
     Timer,
@@ -22,35 +25,50 @@ use nrf52832_hal::{
 fn main() -> ! {
     // instanciate board and timer
     let mut board = DWM1001::take().unwrap();
-    //let mut timer = board.TIMER0.constrain();
-    let mut timer = Timer::new(board.TIMER0); // stolen from blinky
+    let mut timer = board.TIMER0.constrain();
 
     let mut red = board.pins.SPIS_MOSI.into_floating_input().degrade();
     let mut green = board.pins.SPIS_MISO.into_floating_input().degrade();
     let mut blue = board.pins.SPIS_CLK.into_floating_input().degrade();
+    // TODO double check that this is actually unused
+    let mut unused = board.pins.GPIO_12.into_floating_input().degrade();
 
     let channels = pwm::Channels {
         pwm_ch0: red,
         pwm_ch1: green,
         pwm_ch2: blue,
-        pwm_ch3: blue, // TODO added to silence compiler; what would a suitable pin here?
+        pwm_ch3: unused, // TODO added to silence compiler; what would a suitable pin here?
     };
 
     static sequence: [u16; 4] = [9000, 15000, 10000, 0x3333];
 
     let mut pulse = Pwm::new(board.PWM0, channels, pwm::Prescaler::DIV_8);
 
-    pulse.set_decoder(DecoderLoad::Individual, DecoderMode::RefreshCount)
-            .set_wavecounter(WaveCounterMode::Up)
-            .disable_loop()
-            .set_sequence_0(sequence, 0, 0)
-            .start_sequence_0();
+    // TODO change functions so that they return pulse so that they can be daisy chained?
+    pulse.set_decoder(DecoderLoad::Individual, DecoderMode::RefreshCount);
+    pulse.set_wavecounter(WaveCounterMode::Up);
+    pulse.disable_loop();
+    pulse.set_sequence_0(sequence, 0, 0);
+    pulse.start_sequence_0();
 
+    // todo pick sensible value based on cycle length
+    let cycles = 250_000;
     loop {
         // blink red LED for not ready status
-        timer.delay(250_000);
+        delay(&mut timer, cycles);
         board.leds.D11.enable();
-        timer.delay(250_000);
+
+        delay(&mut timer, cycles);
         board.leds.D11.disable();
     }
+}
+
+
+// stolen from blinky
+fn delay<T>(timer: &mut Timer<T>, cycles: u32)
+where
+    T: TimerExt,
+{
+    timer.start(cycles);
+    block!(timer.wait()).unwrap();
 }
